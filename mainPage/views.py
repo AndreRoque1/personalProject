942dbe8f79b38040b3aa521cb9ac6f8d250ee86d
AndreRoque1 <andre.roque@outlook.pt>
2023-05-03 14:26:07 +0100

    upgrades

--- mainPage/views.py
+++ mainPage/views.py
@@ -76,15 +76,3 @@ def login(request):
 
 def signup(request):
     return render(request, 'signup.html')
-
-oauth = OAuth()
-
-oauth.register(
-    "auth0",
-    client_id=settings.AUTH0_CLIENT_ID,
-    client_secret=settings.AUTH0_CLIENT_SECRET,
-    client_kwargs={
-        "scope": "openid profile email",
-    },
-    server_metadata_url=f"https://{settings.AUTH0_DOMAIN}/.well-known/openid-configuration",
-)
