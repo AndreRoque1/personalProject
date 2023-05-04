76efb83ee4a20678296c9ffdf82d0814da39d63d
AndreRoque1 <andre.roque@outlook.pt>
2023-05-04 23:12:33 +0100

    Update models.py

--- mainPage/models.py
+++ mainPage/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
-class utilizadores(models.Model):
-    nome=models.CharField(max_length=100)
-    nickname=models.CharField(max_length=50)
-    email=models.CharField(max_length=100)
+class Utilizadores(models.Model):
+    nome = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+
+    # META CLASS
+    class Meta:
+        verbose_name = 'Utilizadores'
+        verbose_name_plural = 'Utilizadores'
+
     def __str__(self):
         return self.nome
