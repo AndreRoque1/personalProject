2f1162d85f9c9241c1acbb6e583046f5ee350617
AndreRoque1 <andre.roque@outlook.pt>
2023-05-04 23:12:51 +0100

    Update urls.py

--- mainPage/urls.py
+++ mainPage/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -10,4 +12,7 @@ urlpatterns = [
     path("logout/", views.logout, name="logout"),
     path("callback/", views.callback, name="callback"),
     path('signup/', views.signup, name='signup'),
-]
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
